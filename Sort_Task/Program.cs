5bf7c3e04837a5cb22b86733aaaa42caefd7faaa
OlegIvsv <oleg40512@gmail.com>
2022-01-13 20:49:22 +0200

    Добавьте файлы проекта.

---
+++ Sort_Task/Program.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Sort_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<int[]> print = (arr1) =>
+            {
+                foreach (var item in arr1)
+                    Console.Write(item.ToString() + ", ");
+            };
+
+            var people = new Person[]
+            {
+                new(){Age = 23, Name = "Ivan1", Lastname="a", Cash = 10000 },
+                new(){Age = 45, Name = "Ivan2", Lastname="c", Cash = 10 },
+                new(){Age = 32, Name = "Ivan3", Lastname="b", Cash = 780 },
+                new(){Age = 19, Name = "Ivan4", Lastname="e", Cash = 102 },
+                new(){Age = 101, Name = "Ivan5", Lastname="d", Cash = 88 },
+                new(){Age = 47, Name = "Ivan6", Lastname="g", Cash = 1 },
+                new(){Age = 47, Name = "Ivan6", Lastname="f", Cash = 1 },
+                new(){Age = 47, Name = "Ivan6", Lastname="t", Cash = 1 },
+                new(){Age = 47, Name = "Ivan6", Lastname="w", Cash = 1 },
+            };
+
+            //var res = Sorter.QuickRecursive(people, (x, y) => y.Age - x.Age);
+
+            //print(res);
+
+            for (int i = 0; i < 200; ++i)
+            {
+                int[] arr = new int[i];
+                Random rand = new();
+                for (int j = 0; j < arr.Length; j++)
+                {
+                    arr[j] = rand.Next(1000);
+                }
+                int a, b;
+                a = rand.Next(arr.Length);
+                b = rand.Next(a, arr.Length);
+                arr = Sort.Piramidal(arr, a, b, (x, y) => x - y);
+                bool o = IsOrdered(arr, a, b);
+                Console.WriteLine(o);
+                if (!o)
+                {
+                    print(arr);
+                    Console.WriteLine("\n" + a + " " + b);
+                    break;
+                }
+                if (i == 1 || i == 5 || i == 30 || i == 40 || i ==55 )
+                {
+                    print(arr);
+                    Console.WriteLine(a + " " + b);
+                }
+            }
+
+
+            //var sorted = Sort.Piramidal(people,0, (x, y) => x.Lastname.CompareTo(y.Lastname));
+            //print(sorted);
+            //Console.WriteLine(  );
+            //print(people.OrderBy(x => x.Lastname));
+        }
+
+        static bool IsOrdered(int[] arr, int a, int b)
+        {
+            if (arr.Length == 0 || a == b)
+                return true;
+            for (int i = a; i <= b; i++)
+            {
+                if (arr[Math.Min(i + 1, Math.Min(arr.Length - 1, b))] < arr[i])
+                    return false;
+            }
+            return true;
+        }
+        static bool IsOrdered(int[] arr)
+        {
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                if (arr[i + 1] < arr[i])
+                    return false;
+            }
+            return true;
+        }
+    }
+
+    public class Person
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+        public string Lastname { get; set; }
+        public double Cash { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Age} - {Name} {Lastname} - {Cash}";
+        }
+    }
+}
+
+
+public static class Sort
+{
+    public static T[] Piramidal<T>(T[] input, int startIndex, int endIndex, Func<T, T, int> comparer)
+    {
+        if (input == null)
+            throw new ArgumentNullException(nameof(input));
+        if (comparer == null)
+            throw new NullReferenceException(nameof(comparer));
+
+        int rangeL = endIndex - startIndex + 1;
+
+        var array = new T[input.Length];
+        input.CopyTo(array, 0);
+        /* Bringint to a heap */
+        for (int i = startIndex + rangeL / 2 - 1; i >= startIndex; --i)
+            MakeHeap(array, i, startIndex, endIndex, comparer);
+        /* Sorting process */
+        for (int i = startIndex + rangeL - 1; i >= startIndex + 1; --i)
+        {
+            T temp = array[startIndex];
+            array[startIndex] = array[i];
+            array[i] = temp;
+
+            MakeHeap(array, startIndex, startIndex, i - 1, comparer);
+        }
+
+        return array;
+    }
+    private static void MakeHeap<T>(T[] input, int fatherNodeIndex, int startIndex, int border, Func<T, T, int> comparer)
+    {
+        while ((fatherNodeIndex - startIndex) * 2 + 1 <= border - startIndex)
+        {
+            /* Search the largest of two descendants */
+            int maxDescendantIndex = (fatherNodeIndex - startIndex) * 2 + 1 + startIndex;
+            if ((fatherNodeIndex - startIndex) * 2 + 2 + startIndex <= border
+                && comparer(input[(fatherNodeIndex - startIndex) * 2 + 1 + startIndex], input[(fatherNodeIndex - startIndex) * 2 + 2 + startIndex]) < 1)
+                ++maxDescendantIndex;
+            /* Swap if necessary */
+            if (comparer(input[maxDescendantIndex], input[fatherNodeIndex]) > 0)
+            {
+                T temp = input[maxDescendantIndex];
+                input[maxDescendantIndex] = input[fatherNodeIndex];
+                input[fatherNodeIndex] = temp;
+                /* Proceed to descendant */
+                fatherNodeIndex = maxDescendantIndex;
+            }
+            else
+                break;
+        }
+    }
+}
---
+++ Sort_Task/Sorter.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sort_Task
+{
+    public static class Sorter
+    {
+        /* * * * Quick sort | Recursive method * * * */
+        /* Launcher */
+        public static T[] QuickRecursive<T>(T[] input, int startIndex, int endIndex, Func<T, T, int> comparer)
+        {
+            if (input == null)
+                throw new ArgumentNullException(nameof(input));
+            if (startIndex < 0 || startIndex > endIndex)
+                throw new ArgumentException($"Incorrect range:{startIndex}, {endIndex}.");
+            if (comparer == null)
+                throw new NullReferenceException(nameof(comparer));
+
+            var array = new T[input.Length];
+            input.CopyTo(array, 0);
+            QuickRecursiveAlgorithm(array, startIndex, endIndex, comparer);
+            return array;
+        }
+        /* Launcher */
+        public static T[] QuickRecursive<T>(T[] input, Func<T, T, int> comparer)
+            => QuickRecursive(input, 0, Math.Max(0, input.Length - 1), comparer);
+        private static void QuickRecursiveAlgorithm<T>(T[] array, int startIndex, int endIndex, Func<T, T, int> comparer)
+        {
+            void Swap(int indexA, int indexB)
+            {
+                T temp = array[indexA];
+                array[indexA] = array[indexB];
+                array[indexB] = temp;
+            }
+
+            if (endIndex <= startIndex)
+                return;
+
+            int XElemIndex = endIndex;
+            int currentIndex = startIndex;
+
+            for (int i = startIndex; i < endIndex; i++)
+            {
+                if (comparer(array[i], array[XElemIndex]) <= 0)
+                {
+                    Swap(currentIndex, i);
+                    ++currentIndex;
+                }
+            }
+
+            Swap(currentIndex, XElemIndex);
+            QuickRecursiveAlgorithm(array, startIndex, currentIndex - 1, comparer);
+            QuickRecursiveAlgorithm(array, currentIndex + 1, endIndex, comparer);
+        }
+
+        /* * * * Quick sort | Iterative method * * * */
+        /* Launcher */
+        public static T[] QuickIterative<T>(T[] input, int startIndex, int endIndex, Func<T, T, int> comparer)
+        {
+            if (input == null)
+                throw new ArgumentNullException(nameof(input));
+            if (startIndex < 0 || startIndex > endIndex)
+                throw new ArgumentException($"Incorrect range:{startIndex}, {endIndex}.");
+            if (comparer == null)
+                throw new NullReferenceException(nameof(comparer));
+
+            var array = new T[input.Length];
+            input.CopyTo(array, 0);
+            QuickIterativeAlgorithm(array, startIndex, endIndex, comparer);
+            return array;
+        }
+        /* Launcher */
+        public static T[] QuickIterative<T>(T[] input, Func<T, T, int> comparer)
+           => QuickIterative(input, 0, Math.Max(0, input.Length - 1), comparer);
+        private static void QuickIterativeAlgorithm<T>(T[] array, int startIndex, int endIndex, Func<T, T, int> comparer)
+        {
+            void Swap(int indexA, int indexB)
+            {
+                T temp = array[indexA];
+                array[indexA] = array[indexB];
+                array[indexB] = temp;
+            }
+
+            (int start, int end) current = (startIndex, endIndex);
+
+            Stack<(int start, int end)> stack = new();
+            stack.Push(current);
+
+            while (stack.Count != 0) 
+            {
+                current = stack.Pop();
+
+                if (current.end <= current.start)
+                    continue;
+
+                int XElemIndex = current.end;
+                int currentIndex = current.start;
+
+                for (int i = current.start; i < current.end; i++)
+                {
+                    if (comparer(array[i], array[XElemIndex]) <= 0)
+                    {
+                        Swap(currentIndex, i);
+                        ++currentIndex;
+                    }
+                }
+                Swap(currentIndex, XElemIndex);
+
+                stack.Push((current.start, currentIndex - 1));
+                stack.Push((currentIndex + 1, current.end));
+            }
+        }
+
+        /* * * * Piramidal sort * * * */
+        /* All array*/
+        public static T[] Piramidal<T>(T[] input, Func<T,T,int> comparer)
+        {
+            if (input == null)
+                throw new ArgumentNullException(nameof(input));
+            if (comparer == null)
+                throw new NullReferenceException(nameof(comparer));
+
+            var array = new T[input.Length];
+            input.CopyTo(array, 0);
+            /* Bringint to a heap */
+            for (int i = array.Length / 2 - 1; i >= 0; --i)
+                MakeHeap(array, i, array.Length - 1, comparer);
+            /* Sorting process */
+            for (int i = array.Length - 1; i >= 1; --i)
+            {
+                T temp = array[0];
+                array[0] = array[i];
+                array[i] = temp;
+
+                MakeHeap(array, 0, i - 1, comparer);
+            }
+
+            return array;
+        }
+        private static void MakeHeap<T>(T[] input, int fatherNodeIndex, int border, Func<T,T,int> comparer)
+        {
+            while(fatherNodeIndex * 2 + 1 <=  border)
+            {   
+                /* Search the largest of two descendants */
+                int maxDescendantIndex = fatherNodeIndex * 2 + 1;
+                if (fatherNodeIndex * 2 + 2 <= border 
+                    && comparer(input[fatherNodeIndex * 2 + 1], input[fatherNodeIndex * 2 + 2]) < 1)
+                    ++maxDescendantIndex;
+                /* Swap if necessary */
+                if (comparer(input[maxDescendantIndex], input[fatherNodeIndex]) > 0)
+                {
+                    T temp = input[maxDescendantIndex];
+                    input[maxDescendantIndex] = input[fatherNodeIndex];
+                    input[fatherNodeIndex] = temp;
+                    /* Proceed to descendant */
+                    fatherNodeIndex = maxDescendantIndex;
+                }
+                else
+                    break;
+            }
+        }
+
+        /* For ranges */
+        public static T[] Piramidal<T>(T[] input, int startIndex, int endIndex, Func<T, T, int> comparer)
+        {
+            if (input == null)
+                throw new ArgumentNullException(nameof(input));
+            if (startIndex < 0 || startIndex > endIndex)
+                throw new ArgumentException($"Incorrect range:{startIndex}, {endIndex}.");
+            if (comparer == null)
+                throw new NullReferenceException(nameof(comparer));
+
+            int rangeL = endIndex - startIndex + 1;
+
+            var array = new T[input.Length];
+            input.CopyTo(array, 0);
+            /* Bringint to a heap */
+            for (int i = startIndex + rangeL / 2 - 1; i >= startIndex; --i)
+                MakeHeap(array, i, startIndex, endIndex, comparer);
+            /* Sorting process */
+            for (int i = startIndex + rangeL - 1; i >= startIndex + 1; --i)
+            {
+                T temp = array[startIndex];
+                array[startIndex] = array[i];
+                array[i] = temp;
+
+                MakeHeap(array, startIndex, startIndex, i - 1, comparer);
+            }
+
+            return array;
+        }
+        private static void MakeHeap<T>(T[] input, int fatherNodeIndex, int startIndex, int border, Func<T, T, int> comparer)
+        {
+            while ((fatherNodeIndex - startIndex) * 2 + 1 <= border - startIndex)
+            {
+                /* Search the largest of two descendants */
+                int maxDescendantIndex = (fatherNodeIndex - startIndex) * 2 + 1 + startIndex;
+                if ((fatherNodeIndex - startIndex) * 2 + 2 + startIndex <= border
+                    && comparer(input[(fatherNodeIndex - startIndex) * 2 + 1 + startIndex], input[(fatherNodeIndex - startIndex) * 2 + 2 + startIndex]) < 1)
+                    ++maxDescendantIndex;
+                /* Swap if necessary */
+                if (comparer(input[maxDescendantIndex], input[fatherNodeIndex]) > 0)
+                {
+                    T temp = input[maxDescendantIndex];
+                    input[maxDescendantIndex] = input[fatherNodeIndex];
+                    input[fatherNodeIndex] = temp;
+                    /* Proceed to descendant */
+                    fatherNodeIndex = maxDescendantIndex;
+                }
+                else
+                    break;
+            }
+        }
+    }
+}
